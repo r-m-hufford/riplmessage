2850ee21669c373e42cd993a419804562440c526
Xiong Yuan <78838190+xyuan04@users.noreply.github.com>
2021-05-02 14:02:39 -0400

    created page for forgot password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChannelComponent } from './channel/channel.component';
 import { FormsModule } from '@angular/forms';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChannelComponent
+    ChannelComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
