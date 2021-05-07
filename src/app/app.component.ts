40a0c60783a1790b2943d4b91226beb5c4f6a1cb
Xiong Yuan <78838190+xyuan04@users.noreply.github.com>
2021-05-07 10:33:49 -0400

    Doesnt work. Dolio will take a look

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WebsocketService} from './websocket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'riplFrontEnd';
+
+  webSocketService: WebsocketService;
+  greeting: any;
+  name: string;
+
+  ngOnInit() {
+    this.webSocketService = new WebsocketService(new AppComponent());
+  }
+
+  connect(){
+    this.webSocketService._connect();
+  }
+
+  disconnect(){
+    this.webSocketService._disconnect();
+  }
+
+  sendMessage(){
+    this.webSocketService._send(this.name);
+  }
+
+  handleMessage(message){
+    this.greeting = message;
+  }
 }
--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -3,6 +3,7 @@ import { Channel } from '../channel';
 import { ChannelService } from '../channel.service';
 import {Message} from '../message';
 import {HttpErrorResponse} from '@angular/common/http';
+import {webSocket} from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-channel',
@@ -13,6 +14,7 @@ export class ChannelComponent implements OnInit {
   channels: Channel[] = [];
   selectedChannel?: Channel;
   message = '';
+  subject = webSocket('ws://localhost:8080/chat');
 
   constructor(private channelService: ChannelService) { }
 
@@ -27,15 +29,24 @@ export class ChannelComponent implements OnInit {
     );
   }
 
+  sendToServer($event) {
+    this.subject.subscribe();
+    this.subject.next(this.message);
+    this.subject.complete();
+  }
+
   onClick(channel: Channel): void {
     this.selectedChannel = channel;
   }
 
-  submitMessage(): void {
+  submitMessage($event): void {
     let date: Date = new Date();
     let newMessage: Message = {messageBody: this.message, timeStamp: date, channel: this.selectedChannel};
     this.channelService.addMessage(newMessage).subscribe();
     this.selectedChannel?.messages.push(newMessage);
+    this.subject.subscribe();
+    this.subject.next(this.message);
+    this.subject.complete();
     this.message = '';
     this.channelService.updateChannel(this.selectedChannel?.id, this.selectedChannel).subscribe();
   }
