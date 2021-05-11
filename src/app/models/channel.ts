1eac0baea53435a947ab7d2ea4eb7a8ae8f21fab
Ryan M Hufford <ryan@rmhufford.com>
2021-05-11 11:19:31 -0400

    channel interface bug fixed

--- src/app/models/channel.ts
+++ src/app/models/channel.ts
@@ -2,7 +2,7 @@ import {User} from './user';
 import {Message} from './message';
 
 export interface Channel {
-  id?: number;
+  id: number;
   name: string;
   users?: User[];
   messages?: Message[];
